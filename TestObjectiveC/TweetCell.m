97d82c530ec331f32d0a610752fb1369010a55e4
Eldin <eldinsmakic@hotmail.fr>
2021-06-02 19:29:43 +0200

    [Fix]: Was not using the correct initializer

--- TestObjectiveC/TweetCell.m
+++ TestObjectiveC/TweetCell.m
@@ -12,10 +12,19 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     // Initialization code
+    NSLog(@"Hello world");
 }
 
-- (instancetype)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
+- (instancetype)initWithCoder:(NSCoder *)coder {
+    self = [super initWithCoder:coder];
+    NSLog(@"Hello world");
+    return self;
+}
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    NSLog(@"Hello worldss");
+
     if (self) {
 
         UILabel * label = [UILabel new];
@@ -34,9 +43,38 @@
         NSArray<NSLayoutConstraint *> * constraintsView = [NSArray arrayWithObjects:[[label topAnchor] constraintEqualToAnchor: [self.contentView topAnchor ]],[[label leftAnchor] constraintEqualToAnchor: [self.contentView leftAnchor]], nil];
 
         [NSLayoutConstraint activateConstraints:constraintsView];
-    
+
+        self.backgroundColor = UIColor.grayColor;
     }
 
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+
+    if (self) {
+
+        UILabel * label = [UILabel new];
+
+        NSArray<NSLayoutConstraint *> * constraints = [NSArray arrayWithObjects:[[label heightAnchor] constraintEqualToConstant:40], [[label widthAnchor] constraintEqualToConstant:40], nil];
+
+        [NSLayoutConstraint activateConstraints: constraints];
+
+        label.text = @"hello world";
+        label.backgroundColor = UIColor.blueColor;
+        label.translatesAutoresizingMaskIntoConstraints = NO;
+
+        [self.contentView addSubview: label];
+        self.contentView.translatesAutoresizingMaskIntoConstraints = NO;
+
+        NSArray<NSLayoutConstraint *> * constraintsView = [NSArray arrayWithObjects:[[label topAnchor] constraintEqualToAnchor: [self.contentView topAnchor ]],[[label leftAnchor] constraintEqualToAnchor: [self.contentView leftAnchor]], nil];
+
+        [NSLayoutConstraint activateConstraints:constraintsView];
+
+        self.backgroundColor = UIColor.grayColor;
+    }
+    NSLog(@"Hellow");
     return self;
 }
 
